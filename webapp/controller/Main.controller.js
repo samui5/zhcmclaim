f0c6ffa08ffbec1b868982581bfe41acc325e480
suryakantsoni1991 <suryakantsoniofficial@gmail.com>
2020-12-06 18:02:44 +0530

    surya changes

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/model/json/JSONModel",
-	"hcm/claim/util/formatter"
-], function(Controller, JSONModel, formatter) {
+	"hcm/claim/util/formatter",
+	"sap/m/Dialog"
+], function(Controller, JSONModel, formatter,Dialog) {
 	"use strict";
 	return Controller.extend("hcm.claim.controller.Main", {
 		onInit: function() {
@@ -37,6 +38,7 @@ sap.ui.define([
 		_onRouteMatched: function() {
 
 		},
+		aItems: [], // changes by Surya 06.12.2020
 		onAddRow: function() {
 			var date = new Date();
 			// var record = this.getView().getModel("local").getProperty("/record");
@@ -51,12 +53,12 @@ sap.ui.define([
 				"Purpose": "",
 				"Destination": "",
 				"Comments": "",
-				"To_Attachments": []
+				"To_Attachments": [ ] // changes by Surya 06.12.2020
 			};
 			record.Createdate = date.getDate() + "." + (date.getMonth() + 1) + "." + date.getFullYear();
-			var aItems = this.getView().getModel("local").getProperty("/tableData");
-			aItems.push(record);
-			this.getView().getModel("local").setProperty("/tableData", aItems);
+			this.aItems = this.getView().getModel("local").getProperty("/tableData");
+			this.aItems.push(record);
+			this.getView().getModel("local").setProperty("/tableData", this.aItems);
 			this.getView().byId("idonSave").setEnabled(true);
 			this.getView().byId("idonSubmit").setEnabled(false);
 		},
@@ -135,29 +137,133 @@ sap.ui.define([
 				}
 			});
 		},
+		// Start of changes  by Surya 06.12.2020
 		onSelectPhoto: function(oEvent) {
+			debugger;
 			var that = this;
-			// move selected row data to global variable
-			// this.selRow = oEvent.getSource().getBindingContext().getObject();
-			// var relPath = oEvent.getSource().getBindingContext().getPath() + ("/ToPhotos");
-			that.photoPopup = new sap.ui.xmlfragment("hcm.claim.fragments.PhotoUploadDialog", that);
-			that.getView().addDependent(that.photoPopup);
-			// this.ODataHelper.callOData(this.getOwnerComponent().getModel(),
-			// 		relPath, "GET", {}, {}, this)
-			// 	.then(function(oData) {
-			// 		if (!that.photoPopup) {
-			// 			that.photoPopup = new sap.ui.xmlfragment("hcm.claim.fragments.PhotoUploadDialog", that);
-			// 			that.getView().addDependent(that.photoPopup);
-			// 		}
-			// 		var oModelPhoto = new JSONModel();
-			// 		oModelPhoto.setData(oData.results[0]);
-			// 		that.getView().setModel(oModelPhoto, "photo");
-			// 		that.photoPopup.open();
-			// 	}).catch(function(oError) {
-			// 		that.getView().setBusy(false);
-			// 		// var oPopover = that.getErrorMessage(oError);
-			// 	});
+			if (!that.photoPopup) {
+				that.photoPopup = new sap.ui.xmlfragment("hcm.claim.fragments.PhotoUploadDialog", that);
+				that.getView().addDependent(that.photoPopup);
+			}
+			that.photoPopup.open();
+			// get the index of the row for which the attachment button has been clicked
+				var path = oEvent.getSource().mBindingInfos.text.binding.getBindings()[0].getContext().getPath();
+				this.selectedIndex = path.split("/", 3)[2];
 		},
+
+		selectedIndex: null,
+		handleUploadPress: function(oEvent) {
+			debugger;
+			var oFileUploader = sap.ui.getCore().byId("idCoUploader");
+			// if (!oFileUploader.getValue()) {
+			//   sap.m.MessageToast.show("Choose a file first");
+			//   return;
+			// }
+			if (oFileUploader.getValue()) {
+				this.flag = 'U';
+				var file = jQuery.sap.domById(oFileUploader.getId() + "-fu").files[0];
+
+				this.fileName = file.name;
+				this.fileType = file.type;
+
+				var reader = new FileReader();
+				reader.onload = function(e) {
+					var oFile = {};
+					oFile.imgContent = e.currentTarget.result;
+					// getting the attachment content into local json model
+					this.aItems[this.selectedIndex].To_Attachments.push({ Content : oFile.imgContent});
+            //show uploaded picture
+                  var oModelPhoto = new JSONModel();
+                  oModelPhoto.setData(this.aItems[this.selectedIndex]);
+                  this.getView().setModel(oModelPhoto, "photo");
+					//that.savePicToDb(that.fileName, that.fileType, picture);
+					//if picture already exists update the picture
+					// if (that.selRow.Picture==="X") {
+					//   var photoId = that.getView().getModel("photo").getData().id;
+					//   var payload = {
+					//     id: photoId,
+					//     Content: picture,
+					//     name: that.fileName,
+					//     type:that.fileType
+					//   };
+					//   $.post('/updatePhoto', payload)
+					//     .done(function(data, status) {
+					//       sap.m.MessageToast.show("Photo updated");
+					//       debugger;
+					//       var oModelPhoto = new JSONModel();
+					//       oModelPhoto.setData(payload);
+					//       that.getView().setModel(oModelPhoto, "photo");
+					//     })
+					//     .fail(function(xhr, status, error) {
+					//       sap.m.MessageBox.error("Failed to update photo");
+					//     });
+					// } else{
+					// // if picture doesn't exist then create new record
+					//     var payload = {
+					//       CustomerOrderId: that.selRow.id,
+					//       Content: picture,
+					//       Filename: that.fileName,
+					//       Filetype: that.fileType
+					//     }
+					//     that.ODataHelper.callOData(that.getOwnerComponent().getModel(), "/Photos",
+					//                       "POST", {}, payload, that)
+					//       .then(function(oData) {
+					//           sap.m.MessageToast.show("Photo uploaded Successfully");
+					//           // update picture flag in customer orders
+					//           debugger;
+					//     //show uploaded picture
+					//           var oModelPhoto = new JSONModel();
+					//           oModelPhoto.setData(oData);
+					//           that.getView().setModel(oModelPhoto, "photo");
+					//    // update photo flag in customer order
+					//           that2.selRow.Picture = "X";
+					//           var payload = {
+					//             id: that2.selRow.id,
+					//             PhotoValue : that2.selRow.Picture
+					//           };
+					//           $.post('/updatePhotoFlag', payload)
+					//   					.done(function(data, status) {
+					//   						sap.m.MessageToast.show("Data updated");
+					//   					})
+					//   					.fail(function(xhr, status, error) {
+					//   						sap.m.MessageBox.error("Failed to update");
+					//   					});
+					//     // call clear to update the color of the image
+					//             that2.onClear();
+					//           that2.getView().setBusy(false);
+					//         }).catch(function(oError) {
+					//           that2.getView().setBusy(false);
+					//           var oPopover = that.getErrorMessage(oError);
+					//         });
+					//   ;}
+				}.bind(this);
+				reader.readAsDataURL(file);
+			} else if (this.flag === 'C') {
+				var snapId = 'Capture';
+			//	this.savePicToDb(this.attachName,
+			//		"jpeg",
+			//		document.getElementById(snapId).toDataURL())
+			} else {
+				sap.m.MessageToast.show("Upload or capture Picture");
+				return;
+			}
+		},
+      
+		// close photo upload popup
+		handleClosePress: function(oEvent) {
+			debugger;
+			if (!this.photoPopup) {
+				this.photoPopup = new sap.ui.xmlfragment("victoria.fragments.PhotoUploadDialog", this);
+			}
+			var oFileUploader = sap.ui.getCore().byId("idCoUploader");
+			oFileUploader.setValue("");
+			// var oVBox = this.getView().getDependents()[0].getContent()[0].getContent()[0];
+			//oVBox.getItems()[3].destroyLayoutData();
+			// oVBox.getItems()[3].setProperty("content", "");
+			this.photoPopup.close();
+		},
+
+		// End of changes  by Surya 06.12.2020
 		// onUploadChange: function(oEvent) {
 		// 	debugger;
 		// 	var files = oEvent.getParameter("files");
