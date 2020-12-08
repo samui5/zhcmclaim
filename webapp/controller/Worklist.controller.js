3e3c5363d1b9dded1551cce6dba5f9c02f41652f
Anubhav <anubhav.aws@gmail.com>
2020-12-08 17:33:32 +0530

    Worklist View Finished

---
+++ webapp/controller/Worklist.controller.js
@@ -0,0 +1,61 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"hcm/claim/util/formatter"
+], function(Controller, formatter) {
+	"use strict";
+
+	return Controller.extend("hcm.claim.controller.Worklist", {
+		
+		/**
+		 * Called when a controller is instantiated and its View controls (if available) are already created.
+		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+		 * @memberOf hcm.claim.view.Worklist
+		 */
+			onInit: function() {
+				this.oRouter = this.getOwnerComponent().getRouter();
+				//this.oRouter.getRoute("spiderman").attachMatched(this.herculis, this);
+			},
+			onNextItem: function(oEvent){
+				var selectedItem = oEvent.getParameter("listItem");
+				var sPath = selectedItem.getBindingContextPath();
+				var sIndex = sPath.split("/")[sPath.split("/").length - 1];
+				this.oRouter.navTo("main",{
+					claimid: sIndex
+				});
+			},
+			onAdd: function(){
+				this.oRouter.navTo("main",{
+					claimid: "new"
+				});
+			},
+			formatter: formatter
+
+		/**
+		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
+		 * (NOT before the first rendering! onInit() is used for that one!).
+		 * @memberOf hcm.claim.view.Worklist
+		 */
+		//	onBeforeRendering: function() {
+		//
+		//	},
+
+		/**
+		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
+		 * This hook is the same one that SAPUI5 controls get after being rendered.
+		 * @memberOf hcm.claim.view.Worklist
+		 */
+		//	onAfterRendering: function() {
+		//
+		//	},
+
+		/**
+		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
+		 * @memberOf hcm.claim.view.Worklist
+		 */
+		//	onExit: function() {
+		//
+		//	}
+
+	});
+
+});
